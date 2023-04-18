988d6a91f0b3f584a3f2567f06cdf0187d6e19a9
Andras Corda <115708078+R3tr0-andras@users.noreply.github.com>
2023-04-18 11:52:15 +0200

    Add files via upload
    zephyr

---
+++ IMATP5_Andras/IMATP5_andras/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace IMATP5_andras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entrez le nombre N :\n");
+            int n = int.Parse(Console.ReadLine());
+            Triangle(n);
+            string triangle = Triangle(n);
+            Console.WriteLine(triangle);
+        }
+
+        static string Triangle(int n)
+        {
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0 || j == i)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = matrix[i - 1, j - 1] + matrix[i - 1, j];
+                    }
+                }
+            }
+
+            string triangle = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= 1; j++)
+                {
+                    triangle += matrix[i, j] + " ";
+                }
+                triangle += "\n";
+            }
+            return triangle;
+        }
+    }
+}
